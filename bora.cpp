5586b49a04aa9165b238e15a6ef80fde7d130154
Agron Hoxha <agronoxa@hotmail.com>
2021-03-30 15:11:43 +0200

    shtojc

--- bora.cpp
+++ bora.cpp
@@ -8,9 +8,10 @@ int main(){
   for(int i=0; i<=5; i++){
     cout<<"bora hoxha"<<endl;
     cout<<"fron Hoxha"<<endl;
-    cout<<"agron"<<endl;
+    cout<<"agron hoxha"<<endl;
+    cout<<"rejhane lushtaku"<<endl;
 
-    
+
 
   }
 
